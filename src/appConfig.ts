0d86ffcd72b5d345a6e0c1a9dbc9ff1bd46f3222
maymax777 <dev.bugger777@yahoo.com>
2022-10-03 10:17:52 -0400

    chore: app config

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -1,12 +1,12 @@
 export const app: { [key: string]: string } = {
-  name: 'dApp Starter', // web app name
-  title: 'dApp Starter Boilerplate', // website title
-  description: 'dApp Starter Boilerplate by https://github.com/arisac', // description of website
+  name: 'Cyber Kongz Dashboard UI', // web app name
+  title: 'Cyber Kongz Dashboard UI', // website title
+  description: 'Cyber Kongz Dashboard UI by https://github.com/maymax', // description of website
   keywords: 'web3, dapp, ethereum, ethers, wagmi, rainbowkit, nextjs, react, tailwindcss, sass, boilerplate', // keywords for SEO
-  url: 'https://dapp-starter.aris.ac', // website url
-  image: '/icon-512x512.png', // image url for SEO
-  favicon: '/favicon.ico', // favicon url
-  twitter: '@arisdotac', // twitter account
+  url: 'https://cyber-kongz-admin-ui-demo.vercel.app/', // website url
+  image: '/favicon.png', // image url for SEO
+  favicon: '/favicon.png', // favicon url
+  twitter: '@maymax777', // twitter account
   themeColor: '#FFFFFF', // theme color
   themeColorDark: '#000000', // theme color dark
 }
